77da0e8d7dec066148b0ed8e4b42153c31c9021d
Yogita Garud <garudyogitap@gmail.com>
2019-02-12 13:50:26 -0800

    Run script to get repos and build environment

---
+++ run-setup.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+git clone https://github.com/01org/processor-trace.git
+git clone https://github.com/intelxed/mbuild.git mbuild
+git clone https://github.com/intelxed/xed
+
+cd xed
+mkdir obj
+cd obj
+../mfile.py
+sudo ../mfile.py --prefix=/usr/local install
+
+cd processor-trace
+cmake .
+make
+sudo make install
+sudo ldconfig
+
+https://mirrors.edge.kernel.org/pub/linux/kernel/v4.x/
+tar -xf Downloads/linux-4.15.tar.xz
+cd linunx-4.15
+make -C tools/perf/
